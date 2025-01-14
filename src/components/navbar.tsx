be939895a4e6f7869bd88c55edee06c67d633399
mhmdrazn <razan4944@gmail.com>
2025-01-14 14:48:47 +0700

    feat:footer

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,18 +10,9 @@ import { copyToClipboard } from '../../lib/utils';
 export default function navbar() {
     return (
         <footer className='flex w-full px-4 py-12 md:px-8 lg:px-16 justify-center items-center bg-dark'>
-            <div className='flex max-w-3xl w-full justify-between'>
-                <div className='flex flex-col gap-8'>
-                    <div className='flex flex-col gap-6'>
-                        <Image src='/mark.svg' alt='logo' width={60} height={42.06}/>
-                        <p className='text-sm text-white/50'>Design, Develop, and Delight <br/>everyone with Ideas</p>
-                    </div>
-                    <p className='font-thin text-sm text-center md:text-left'>
-                        &copy; 2025 Muhammad Razan. All rights reserved.
-                    </p>
-                </div>
-
-                <div className='flex flex-col justify-between items-end'>        
+            <div className='flex flex-col max-w-3xl w-full gap-4 justify-between'>
+                <div className='flex flex-col md:flex-row md:justify-between justify-center items-center gap-6 md:gap-0 pw-full'>
+                    <Image src='/mark.svg' alt='logo' width={60} height={42.06}/>
                     <div className='flex gap-4'>
                         <Link 
                             href="https://linkedin.com/in/muhammadrazan" 
@@ -60,11 +51,23 @@ export default function navbar() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
                         </button>
                     </div>
+                </div>
+                
+
+                <div className='flex flex-col md:flex-row gap-8 justify-center items-center md:justify-between md:items-start'>        
+                    <div className='flex flex-col justify-around items-start w-fit md:gap-20'>
+                        <p className='text-sm text-white/50 md:text-left text-center'>
+                            Design, Develop, and Delight <br/>everyone with Ideas
+                        </p>
+                        <p className='hidden md:block font-thin text-sm pt-2 text-center md:text-left'>
+                            &copy; 2025 Muhammad Razan. All rights reserved.
+                        </p>
+                    </div>
                     
-                    <div className='flex gap-12'>
-                        <div className='flex flex-col justify-end items-end gap-2'>
+                    <div className='flex flex-col md:flex-row justify-center gap-8 md:gap-12'>
+                        <div className='flex flex-col justify-center items-center md:justify-end md:items-end gap-2'>
                             <p className='font-medium'>Actions</p>
-                            <div className='flex flex-col justify-end items-end gap-1'>
+                            <div className='flex flex-col justify-center items-center md:justify-end md:items-end gap-1'>
                                 <Link href={"https://drive.google.com/file/d/1Sgtvdd4dA-lvJ9cCt0pM-K7KG7gZisN1/view?usp=drive_link"} className="anim text-sm text-white/40 hover:text-white">
                                     Resume
                                 </Link>
@@ -74,24 +77,33 @@ export default function navbar() {
                                 <Link href={"https://cal.com/hola-razan"} className="anim text-sm text-white/40 hover:text-white">
                                     Meet with me!
                                 </Link>
-                            </div>
+                            </div>  
                         </div>
-                        <div className='flex flex-col justify-end items-end gap-2'>
+                        <div className='flex flex-col justify-center items-center md:justify-end md:items-end gap-2'>
                             <p className='font-medium'>Quick Links</p>
-                            <div className='flex flex-col justify-end items-end gap-1'>
+                            <div className='flex flex-col justify-center items-center md:justify-end md:items-end gap-1'>
                                 <Link href={"/"} className="anim text-sm text-white/40 hover:text-white">
                                     Home sweet home
+                                </Link>
+                                <Link href={"/exp"} className="anim text-sm text-white/40 hover:text-white">
+                                    Experience & Journey 
                                 </Link>
                                 <Link href={"/projects"} className="anim text-sm text-white/40 hover:text-white">
                                     Featured Projects
                                 </Link>
-                                <Link href={"/exp"} className="anim text-sm text-white/40 hover:text-white">
-                                    Experience & Journey 
+                                <Link href={"/projects"} className="anim text-sm text-white/40 hover:text-white">
+                                    Retrospective
+                                </Link>
+                                <Link href={"/projects"} className="anim text-sm text-white/40 hover:text-white">
+                                    My <span className='text-white/60'><i>Uneg-Uneg</i></span>
                                 </Link>
                             </div>
                         </div>
                     </div>
                 </div>
+                <p className='md:hidden font-thin text-sm text-center md:text-left py-12'>
+                    &copy; 2025 Muhammad Razan. All rights reserved.
+                </p>
             </div>
         </footer>
     )
