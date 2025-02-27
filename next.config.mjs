2de478bdda7a8649bd1c14a935d48b2060113e5e
mhmdrazn <razan4944@gmail.com>
2025-02-27 07:33:25 +0700

    feat: add direct link

--- next.config.mjs
+++ next.config.mjs
@@ -5,4 +5,33 @@ const nextConfig = {
     }
 };
 
+module.exports = {
+    async redirects() {
+        return [
+        {
+            source: '/',
+            destination: 'https://drive.google.com/file/d/1WVJ5-pqrk1eJ-9xwmy85yQmLBzwmlbSm/view?usp=sharing',
+            permanent: true,
+            has: [
+                {
+                    type: 'host',
+                    value: 'portfolio-mhmdrazn.com',
+                },
+            ],
+        },
+        {
+            source: '/',
+            destination: 'https://meet.google.com/puk-viay-guc',
+            permanent: true, 
+            has: [
+                {
+                    type: 'host',
+                    value: 'meet-mhmdrazn.com',
+                },
+            ],
+        },
+        ];
+    },
+};
+
 export default nextConfig;
